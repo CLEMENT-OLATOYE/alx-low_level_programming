734ca59050e6916fb20a2764b7cbd63488041911
olatoye clement <olatoyeclement@gmail.com>
2023-06-18 18:57:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry point of a C program that prints a line to the standard error
  *
@@ -9,5 +8,5 @@
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+return (1);
 }
