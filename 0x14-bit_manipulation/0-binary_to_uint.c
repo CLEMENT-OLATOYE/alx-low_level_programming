446e63225910310722a5b88b365b3fd3684b6ab7
CLEMENT-OLATOYE <olatoyeclement@gmail.com>
2023-12-18 09:24:08 -0800

    0x14

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a string of '0' and '1' characters
+ *
+ * Description
+ * This function converts a binary string to an unsigned integer.
+ * The binary string should consist of '0' and '1' characters only.
+ *
+ * b>>>The binary string to be converted
+ *
+ * Return: The converted unsigned integer.
+ * If there are non-binary characters in the string or if b is NULL, returns 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int length = 0;
+	int base = 1;
+
+	if (!b)
+		return (0);
+	while (b[length] != '\0')
+		length++;
+	for (length--; length >= 0; length--, base *= 2)
+	{
+		if (b[length] != '0' && b[length] != '1')
+			return (0);
+		if (b[length] & 1)
+			result += base;
+	}
+	return (result);
+}#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a string of '0' and '1' characters
+ *
+ * Description
+ * This function converts a binary string to an unsigned integer.
+ * The binary string should consist of '0' and '1' characters only.
+ *
+ * b>>>The binary string to be converted
+ *
+ * Return: The converted unsigned integer.
+ * If there are non-binary characters in the string or if b is NULL, returns 0.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int length = 0;
+	int base = 1;
+
+	if (!b)
+		return (0);
+	while (b[length] != '\0')
+		length++;
+	for (length--; length >= 0; length--, base *= 2)
+	{
+		if (b[length] != '0' && b[length] != '1')
+			return (0);
+		if (b[length] & 1)
+			result += base;
+	}
+	return (result);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit at a specified index to 0.
+ * @n: A pointer to an unsigned long integer.
+ * @index: The index of the bit to clear (0 to 63).
+ *
+ * Description: This function sets the bit at the given index in 'n' to 0, effectively clearing it.
+ *
+ * Return: 1 if the operation was successful, -1 if the index is out of range.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int val;
+
+	if (index > 63)
+		return (-1);
+
+	val = 1 << index;
+
+	if (*n & val)
+		*n ^= val;
+
+	return (1)
+}
